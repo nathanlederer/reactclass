bb777d0e23533254705d4315261277aa5fa8626b
Nathan <nlederer@ets.org>
2019-11-18 12:56:50 -0500

    Create index.js

---
+++ HW1/index.js
@@ -0,0 +1,7 @@
+// Set up the React app from scratch
+// Render an App component (defined in a separate file)
+// Inside App, render:
+  // 1. A Navbar component
+  // 2. A MainContent component
+  // 3. A Footer component
+
