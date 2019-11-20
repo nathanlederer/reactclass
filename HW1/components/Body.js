b45ae4017d4052ca700e0216c10b571453a65960
Nathan <nlederer@ets.org>
2019-11-19 22:48:54 -0500

    Add files via upload

---
+++ HW1/components/Body.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Body()
+{
+    return(
+        <main><h3>This is the body html</h3></main>
+    )
+}
+
+export default Body
---
+++ HW1/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Header()
+{
+    return(
+        <header>HEAD html here</header>
+    )
+}
+
+export default Header
