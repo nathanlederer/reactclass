b24f714c257a7a316696157374f999d1d9f8b7cf
Nathan <nlederer@ets.org>
2019-11-19 22:47:24 -0500

    Add files via upload

---
+++ HW1/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Body from "./components/Body"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
+function App()
+{
+    return(
+        <div>
+        <Header />
+        <Body />
+        <Footer />
+        </div>
+    )
+    
+}
+
+export default App
