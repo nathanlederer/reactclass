44b2dba92fef4ea4c4f5995e3c34af6180d1b4a6
Nathan <nlederer@ets.org>
2020-01-11 23:52:47 -0500

    Add files via upload

---
+++ HW7A/ToDoItem.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function TodoItem(props) {
+    const mystyle = {
+        textDecoration: "line-through"
+    };
+
+    return (
+        <div className="todo-item">
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id) }
+            />
+            <p style={props.item.completed ? mystyle : null }>props.item.text</p>
+        </div>
+    )
+}
+
+export default TodoItem
